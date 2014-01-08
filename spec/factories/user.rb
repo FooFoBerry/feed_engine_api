57da0e1f7bad2cc2d074b2aadeefd60e38941d06
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-08 13:56:06 -0700

    complete get/users

--- spec/factories/user.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do 
-  factory :user do 
-    uid "1986"
-    nickname "Foo"
-    name "FooFo Berry"
-    image_url "http://freshhealthyvending.com/wp-content/uploads/2013/07/Gooseberries-1.jpg"
-    github_url "http://github.com/srt32"
-  end
-end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,10 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    uid "MyString"
-    nickname "MyString"
-    name "MyString"
-    image_url "MyString"
-    github_url "MyString"
+FactoryGirl.define do 
+  factory :user do 
+    uid "1986"
+    nickname "Foo"
+    name "FooFo Berry"
+    image_url "http://freshhealthyvending.com/wp-content/uploads/2013/07/Gooseberries-1.jpg"
+    github_url "http://github.com/srt32"
   end
 end
+
