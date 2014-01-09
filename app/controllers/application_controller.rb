7ecd65211d71f715cc83cc86c0ae63f6f68277a6
Simon Taranto <simon.taranto@gmail.com>
2014-01-08 19:23:34 -0700

    implement User model and complete current_user method
    add User model and implement current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  def current_user
+    @current_user ||= User.new(:id => cookies[:user_id])
+  end
+
 end
