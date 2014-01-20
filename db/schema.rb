7689557bce121c36bfba3fa22e75ae624b19f554
Simon Taranto <simon.taranto@gmail.com>
2014-01-19 18:01:31 -0700

    accept email, username, name from costner app when creating new commits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114081854) do
+ActiveRecord::Schema.define(version: 20140120005209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140114081854) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "repo_id"
+    t.string   "email"
+    t.string   "name"
   end
 
   add_index "commits", ["repo_id"], name: "index_commits_on_repo_id", using: :btree
