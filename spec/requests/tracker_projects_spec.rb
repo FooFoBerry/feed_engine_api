06940c492aaae91ab2e6c9004f8e6297383ee9db
Simon Taranto <simon.taranto@gmail.com>
2014-01-22 07:57:21 -0700

    test for tracker_projects end point

---
+++ spec/requests/tracker_projects_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Tracker Projects API" do
+  describe "GET /projects/:project_id/tracker_projects" do
+    # :TODO: VCR
+    it "returns all the project's tracker projects" do
+      project = FactoryGirl.create :project
+      tracker_project = FactoryGirl.create :tracker_project, project_id: project.id
+
+      get "/api/v1/projects/#{project.id}/tracker_projects"
+
+      expect(response.status).to eq 200
+      body = JSON.parse(response.body)
+      tracker_projects = body.map { |tracker_project| tracker_project["id"] }
+
+      expect(tracker_projects).to match_array(["#{tracker_project.id}"])
+    end
+  end
+end
