554d72c4d87f8bf47dcb0ca38c45a81119229951
Simon Taranto <simon.taranto@gmail.com>
2014-01-22 09:48:20 -0700

    WIP, add test for POST tracker_projects

--- spec/requests/tracker_projects_spec.rb
+++ spec/requests/tracker_projects_spec.rb
@@ -17,4 +17,20 @@ describe "Tracker Projects API" do
       expect(tracker_projects).to match_array([tracker_project.id])
     end
   end
+
+  describe "POST /projects/:project_id/tracker_projects" do
+
+    before :each do
+      @project = FactoryGirl.create(:project)
+      @tracker_project_params = FactoryGirl.build(:tracker_project).as_json
+    end
+
+    it "responds with 201" do
+      post "/api/v1/projects/#{@project.id}/tracker_projects",
+                                          { :tracker_project => @tracker_project_params },
+                                          { "HTTP_ACCEPT" => "application/json" }
+
+      expect(response.status).to eq 201
+    end
+  end
 end
