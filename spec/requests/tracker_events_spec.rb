27b072a8c8f24d79f146f3f0510c13ae3fb072e6
Simon Taranto <simon.taranto@gmail.com>
2014-01-21 22:54:47 -0700

    WIP, first test written

---
+++ spec/requests/tracker_events_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+describe "Tracker Events API" do
+  describe "GET /projects/:project_id/tracker_events" do
+    it "returns the project's tracker events" do
+      project = FactoryGirl.create :project, name: "Tracking Puffy"
+      tracker_event = FactoryGirl.create :tracker_event, project_id: project.id,
+                                                         story_id: 100
+      tracker_event = FactoryGirl.create :tracker_event, story_id: 200
+
+      get "api/v1/projects/#{project.id}/tracker_events"
+
+      expect(response.status).to eq 200
+      body = JSON.parse(response.body)
+      tracker_events = body.map { |te| te["story_id"] }
+
+      expect(tracker_events).to match_array(["100"])
+    end
+  end
+end
