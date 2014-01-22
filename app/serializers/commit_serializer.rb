045da4b989597376c56d49304f2dabb03048d7f1
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-22 05:00:52 -0700

    add additions/deletions

--- app/serializers/commit_serializer.rb
+++ app/serializers/commit_serializer.rb
@@ -1,7 +1,7 @@
 class CommitSerializer < ActiveModel::Serializer
   attributes :id, :message, :username, :email, :repo_id,
              :created_at, :commit_hash, :tiny_hash, :creation_date,
-             :name
+             :name, :additions, :deletions
 
   def tiny_hash
     commit_hash[0...6]
@@ -9,5 +9,21 @@ class CommitSerializer < ActiveModel::Serializer
 
   def creation_date
     created_at.to_i * 1000
+  end
+
+  def stats
+    @stats ||= Octokit.commit(repo_url, commit_hash).stats
+  end
+
+  def repo_url
+    repo.github_url
+  end
+
+  def additions
+    stats.additions
+  end
+
+  def deletions
+    stats.deletions
   end
 end
