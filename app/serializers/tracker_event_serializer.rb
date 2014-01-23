c5db3469dc14de6cc712b11ecfbca02e64c6a223
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-23 01:09:13 -0700

    add js timestamp output

--- app/serializers/tracker_event_serializer.rb
+++ app/serializers/tracker_event_serializer.rb
@@ -1,3 +1,8 @@
 class TrackerEventSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :story_url, :message, :kind, :user_name, :story_id,
+             :change_type, :story_title, :user_initials, :creation_date
+
+  def creation_date
+    object.created_at.to_i * 1000
+  end
 end
