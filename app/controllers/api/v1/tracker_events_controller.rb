eb1f7ca663e5f5728a43daaea7bd975f68a9ee7c
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-23 01:44:13 -0700

    add Pusher and push method

--- app/controllers/api/v1/tracker_events_controller.rb
+++ app/controllers/api/v1/tracker_events_controller.rb
@@ -4,7 +4,7 @@ module Api
 
       def index
         project = Project.find(params[:project_id])
-        tracker_events = project.tracker_events 
+        tracker_events = project.tracker_events
         render json: tracker_events, status: 200
       end
 
@@ -12,6 +12,7 @@ module Api
         tracker_project = TrackerProject.find_by(:pt_project_id => params[:tracker_event][:pt_project_id])
         tracker_event = tracker_project.tracker_events.new(tracker_event_params)
         if tracker_event.save
+          push(tracker)
           render json: tracker_event, status: 201
         else
           render json: tracker_event_errors(tracker_event), status: 418
@@ -34,6 +35,14 @@ module Api
       def tracker_event_errors(tracker)
         messages = tracker.errors.messages
         errors_hash = {"errors" => messages}
+      end
+
+      def push(tracker)
+        data = TrackerEventSerializer.new tracker
+        tracker.tracker_project.projects.map(&:id).each do |id|
+          Pusher["project_#{id}"].trigger("tracker_notification",
+                                          :data => data.as_json)
+        end
       end
     end
   end
